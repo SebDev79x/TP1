118045c0a9f234b4895937c648110ee142fe94ec
SebDev79x <sebastien.mazur@wanadoo.fr>
2022-05-11 13:57:01 +0200

    Structure ressemblant à la maquette

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { ScrollView } from 'react-native';
+const data = require('./tp.json')
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {
+        data.map((scientist) => {
+          return <View style={styles.block}>
+            <Image style={styles.pic}
+              source={scientist.img}></Image>
+            <View style={styles.smallBlock}>
+              <Text style={[styles.styleText1, styles.styleText]}>{scientist.title}</Text>
+
+              <ScrollView> <Text style={[styles.styleText2, styles.styleText]}>{scientist.desc} </Text></ScrollView>
+            </View>
+          </View>
+        })
+      }
     </View>
   );
 }
@@ -14,7 +26,42 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
+  block: {
+    flexDirection: 'row',
+    flex: 1,
+    padding: '10px',
+
+  },
+  pic: {
+    alignItems:'start',
+    flex: .25,
+    height: '100px',
+    width: '100px',
+    marginTop:'10px'
+  },
+  smallBlock: {
+    flex: .75,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '10px'
+
+  },
+  styleText: {
+    alignItems: 'center',
+/*     flex:1
+ */  },
+  styleText1: {
+    /*     flex:.5,
+     */
+    fontSize: '15px',
+  },
+  styleText2: {
+    /*     flex:.5,
+     */
+    fontSize: '10px',
+
+  }
 });
